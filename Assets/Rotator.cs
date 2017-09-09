8f3faaa5a07b7b5ea07ed5c2059a7e64d143044d
Vitaly Sharapov <akaredsc2@gmail.com>
2017-09-09 13:39:40 +0300

    Add pick up objects that rotate

---
+++ Assets/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private readonly Vector3 _rotation = new Vector3(15, 30, 45);
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+}
